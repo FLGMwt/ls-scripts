5c8633a24498c0ceeaafedff631d5c7474fd15f5
Ryan Stelly <ryan.stelly@live.com>
2018-10-29 13:07:23 -0500

    handle no package.json

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ const fs = require("fs");
 
 const readFile = util.promisify(fs.readFile);
 
-readFile("./package.json").then(manifest => {
-  const { scripts } = JSON.parse(manifest);
+readFile("./package.json")
+  .then(manifest => {
+    const { scripts } = JSON.parse(manifest);
 
-  if (!scripts) {
-    console.log("no scripts found");
-    return;
-  }
+    if (!scripts) {
+      console.log("No scripts found in package.json");
+      return;
+    }
 
-  const message = Object.keys(scripts)
-    .map(key => `  ${key}: "${scripts[key]}"`)
-    .join("\n");
+    const message = Object.keys(scripts)
+      .map(key => `  ${key}: "${scripts[key]}"`)
+      .join("\n");
 
-  console.log("Found the following scripts:");
-  console.log(message);
-});
+    console.log("Found the following scripts:");
+    console.log(message);
+  })
+  .catch(() => console.log("Could not find package.json"));
